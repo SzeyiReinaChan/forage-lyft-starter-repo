0082a00a42e149e9d5533ee675434eadd3c9a9c8
szeyireinachan <szeyic@gmail.com>
2022-06-12 21:29:16 -0400

    Correction: Added Module 3 Unit Testing & fixed bugs

--- test/test_battery/test_spindler_battery.py
+++ test/test_battery/test_spindler_battery.py
@@ -1,24 +1,6 @@
 import unittest
 from datetime import datetime
-
-from battery.nubbinBattery import NubbinBattery
 from battery.spindlerBattery import SpindlerBattery
-
-
-class TestNubbinBattery(unittest.TestCase):
-    def test_needs_services(self):
-        today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 6)
-
-        battery = NubbinBattery(today, last_service_date)
-        self.assertTrue(battery.needs_service())
-
-    def test_not_need_service(self):
-        today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 1)
-
-        battery = NubbinBattery(today, last_service_date)
-        self.assertFalse(battery.needs_service())
 
 
 class TestSpindlerBattery(unittest.TestCase):
